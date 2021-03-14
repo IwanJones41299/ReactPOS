74df1d039e6d8b247b7f5d057fbb71ccd60497e7
IwanJones41299 <iwanjones41299@gmail.com>
2021-03-14 18:01:10 +0000

    working on login form UX

--- client/src/components/Screens/HomeScreen.jsx
+++ client/src/components/Screens/HomeScreen.jsx
@@ -1,33 +1,20 @@
-import React from 'react';
-import { Button, Card, Col, Container, Row } from 'react-bootstrap';
-import { isMobile } from 'react-device-detect';
+import React from "react";
+import { Button, Card, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { isMobile } from "react-device-detect";
 
 const HomeScreen = () => {
-
-    if (isMobile) {
-        return (
-            <Container fluid className="homeContainer-m text-center">
-                <h3>Company POS System</h3>
-                <Card.Text style={{ fontSize: "1.4em" }}>
-                    Sign in to access system
-                        </Card.Text>
-                <Button variant="primary" className="login-btn">Login</Button>
+    if(isMobile){
+        return(
+            <Container fluid>
+                <h1>Mobile</h1>
             </Container>
         )
-    } else {
-        return (
-            <Container fluid className="homeContainer-b">
-                <Row>
-                    <Col className="col-3"></Col>
-                    <Col className="col-6 title">
-                        <h3>Company POS System</h3>
-                        <Card.Text style={{ fontSize: "1.4em" }}>
-                            Sign in to access system
-                        </Card.Text>
-                        <Button variant="primary" className="login-btn">Login</Button>
-                    </Col>
-                    <Col className="col-3"></Col>
-                </Row>
+    }else{
+        return(
+            <Container fluid className="homeContainer-b text-center">
+                <h3 className="homeTitle">Company POS System</h3>
+                <Card.Text style={{ fontSize: "1.4em", color: "#ffffff" }}>Welcome Team!</Card.Text>
+                <Card.Text style={{ fontSize: "1.4em", color: "#ffffff" }}>Sign in to access system</Card.Text>
             </Container>
         )
     }
--- client/src/components/Screens/LoginScreen.jsx
+++ client/src/components/Screens/LoginScreen.jsx
@@ -1,11 +1,62 @@
-import React from 'react'
+import React from "react";
+import { Button, Card, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { isMobile } from "react-device-detect";
 
 const LoginScreen = () => {
-    return (
-        <div>
-            <h1>Login Page</h1>
-        </div>
-    )
-}
 
-export default LoginScreen
+    const Login = (e) => {
+        e.preventDefault(); //to stop page refreshing
+        console.log('User Logged in'); //testing purpose only, delete for production
+    }
+
+    if (isMobile) {
+    return (
+      <>
+        <Container fluid className="homeContainer-m text-center">
+          <h3>Company POS System</h3>
+          <h1>LOGO</h1>
+        </Container>
+      </>
+    );
+    } else {
+    return (
+      <>
+        <Container fluid className="homeContainer-b">
+          <Row>
+            <Col className="col-3"></Col>
+            <Col className="col-3 title text-center">
+            <h3>Company POS System</h3>
+            {/* Company Logo here */}
+            <h1 className="pt-3">LOGO</h1> 
+            </Col>
+            <Col className="col-3 loginForm">
+                <Form onSubmit={Login}>
+                    <Form.Group>
+                        <InputGroup>
+                            <InputGroup.Prepend>
+                            {/* Add username image */}
+                                <InputGroup.Text>Login</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <Form.Control type="text" placeholder="username..." />
+                        </InputGroup>
+                    </Form.Group>
+                    <Form.Group>
+                        <InputGroup>
+                            <InputGroup.Prepend>
+                            {/* Add password image */}
+                                <InputGroup.Text>Password</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <Form.Control type="password" placeholder="username..." />
+                        </InputGroup>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="btn-block login-btn" style={{marginTop: "2rem"}}>Login</Button>
+                </Form>
+            </Col>
+          </Row>
+        </Container>
+      </>
+    );
+  }
+};
+
+export default LoginScreen;
